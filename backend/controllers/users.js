844ecdc3f2ba4e7f4533678e25a91ab5d641fff8
kelsonnono <kelsonnono@gmail.com>
2026-01-31 19:43:35 +0800

    初階任務

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,6 +6,7 @@ const { dataSource } = require('../db/data-source')
 const logger = require('../utils/logger')('UsersController')
 const generateJWT = require('../utils/generateJWT')
 
+
 const passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,16}/
 
 function isUndefined (value) {
